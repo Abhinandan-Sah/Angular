44a84bad7e2386cd4e65b83976d6b132ecb8e536
Abhinandan Sah <abhinandansah222@gmail.com>
2026-04-18 17:59:57 +0530

    update

--- Day1/online-order-tracking-system/src/app/app.routes.ts
+++ Day1/online-order-tracking-system/src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
-    title: 'Home | Online Order Tracking System'
-  },
+  { path: '', pathMatch: 'full', redirectTo: 'orders' },
   {
     path: 'orders',
     loadComponent: () => import('./orders/orders.component').then((m) => m.OrdersComponent),
     title: 'Orders | Online Order Tracking System'
   },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: 'orders' }
 ];
